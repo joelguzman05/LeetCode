49ea1a3ac61c7df626faa04c8d170e25c386b6cf
Joel <ing.joel.guzman.05@gmail.com>
2025-01-29 09:42:08 -0400

    Add 021 Merge Two Sorted Lists solution

---
+++ 021-Merge-Two-Sorted-List.java
@@ -0,0 +1,39 @@
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+
+class Solution {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode curr = new ListNode();
+        ListNode dummy = curr;
+        ListNode l1 = list1, l2 = list2;
+        while (l1 != null && l2 != null) {
+            if (l1.val > l2.val) {
+                curr.next = l2;
+                l2 = l2.next;
+            } else {
+                curr.next = l1;
+                l1 = l1.next;
+            }
+            curr = curr.next;
+        }
+
+        if (l1 != null) {
+            curr.next = l1;
+        } else if (l2 != null) {
+            curr.next = l2;
+        }
+
+        return dummy.next;
+    }
+}
