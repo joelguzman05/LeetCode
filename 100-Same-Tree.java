2124194d1fac64bc0d5614902ed3779d00113c16
Joel <ing.joel.guzman.05@gmail.com>
2025-01-29 18:28:34 -0400

    Update 100 isSameTree method

--- 100-Same-Tree.java
+++ 100-Same-Tree.java
@@ -17,10 +17,10 @@ class Solution {
     public boolean isSameTree(TreeNode p, TreeNode q) {
         if (p == null && q == null) {
             return true;
-        } else if (p != null && q != null && p.val == q.val) {
-            return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
-        } else {
-            return false;
         }
+        if (p != null && q != null && p.val == q.val) {
+            return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+        }
+        return false;
     }
 }
